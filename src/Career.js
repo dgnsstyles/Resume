fc093dc420d2b71d9b53f378739d6e16fda8d4de
Usuario <usuario@MacBook-Pro-de-Usuario.local>
2017-06-16 22:11:18 -0400

    firt resume commit

---
+++ src/Career.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Career extends Component {
+	render() {
+		return (
+		<div className="summary">
+                    <p>I'm a Web Developer that Designs, prototipe your product from start to finish, in my carrer, I made both coding and design in the real time applications, and get the final result that you want. My Experience is always a plus, working with clients and diferents Start-ups. Always inovate and try new tecnologies
+                    </p>
+                </div>
+		);
+	}
+}
+
+export default Career;
